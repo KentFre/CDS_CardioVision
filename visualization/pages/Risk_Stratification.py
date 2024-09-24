598d959f6a16d4a159e7c28439ff42550ff9e548
KentFre <kent.fredriksdotter@stud.ki.se>
2024-09-24 15:51:39 +0200

    Created initial CardioVision.py and page files
    CardioVision is the main streamlit file, starting the dashboard. .py files in the pages folder hold the content of the different tabs / pages.

---
+++ visualization/pages/Risk_Stratification.py
@@ -0,0 +1,14 @@
+# This file will cover the risk stratification tab
+
+import streamlit as st
+
+# Set the page configuration
+st.set_page_config(
+    page_title="CardioVision",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    page_icon="👋",
+)
+
+# Set the main title of the dashboard
+st.title("Risk Stratification")
